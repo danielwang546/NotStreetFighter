2342c9bb19246a601984fa32111f602fae296e17
NathanDuggal <39681791+NathanDuggal@users.noreply.github.com>
2021-05-27 11:16:53 -0700

    Better Harder Faster Stronger animations

--- Main.java
+++ Main.java
@@ -4,30 +4,8 @@ import javax.imageio.ImageIO;
 import java.io.*;
 
 
-public class Main extends JFrame {
-  private Main(String arg){
-	  System.out.println();
-    if (arg == null ) {
-        arg = "Person.png";
-    } 
-    JPanel panel = new JPanel(); 
-    panel.setSize(500,640);
-    panel.setBackground(Color.CYAN); 
-    try {
-      Image image = ImageIO.read(new File(arg));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    ImageIcon icon = new ImageIcon(arg); 
-    JLabel label = new JLabel(); 
-    label.setIcon(icon); 
-    panel.add(label);
-    
-    this.getContentPane().add(panel); 
-  }
-
-  
+public class Main {
   public static void main(String[] args) {
-      new Main(args.length == 0 ? null : args[0]).setVisible(true); 
+      NotStreetFighter.main(args);
   }
 }
